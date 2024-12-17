277d7b55bec84f5099f841e8f63e1acf85658f97
Alimul Khakim <alimul.khakim@autobahn-security.com>
2024-12-17 19:37:54 +0700

    feat: add main toolbar in the left

--- src/components/toolbars/main-toolbar/MainToolbar.tsx
+++ src/components/toolbars/main-toolbar/MainToolbar.tsx
@@ -1,10 +1,23 @@
-import { Button } from "antd";
+import {
+  PencilIcon,
+  Square2StackIcon,
+  CircleStackIcon,
+  CursorArrowRaysIcon,
+} from "@heroicons/react/24/outline";
+import { FloatButton } from "antd";
 
 const MainToolbar = () => {
   return (
-    <div className="flex">
-      <Button type="primary">Antd button</Button>
-    </div>
+    <FloatButton.Group
+      shape="square"
+      placement="top"
+      className="top-[2px] left-[2px] w-fit h-full"
+    >
+      <FloatButton icon={<Square2StackIcon />} />
+      <FloatButton icon={<CircleStackIcon />} />
+      <FloatButton icon={<CursorArrowRaysIcon />} />
+      <FloatButton icon={<PencilIcon />} />
+    </FloatButton.Group>
   );
 };
 
